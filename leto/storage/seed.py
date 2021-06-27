8905ee7b6262e7398a0b14b5889088376c59ae7b
Ernesto Luis Estevanell Valladares <ernesto.estevanell@iberant.es>
2021-06-27 14:31:02 -0400

    Added coref resolution for english

--- leto/storage/seed.py
+++ leto/storage/seed.py
@@ -1,15 +1,37 @@
+from typing import Iterable
+import spacy
 import wikipedia
 from leto.model import Entity, Relation
 from leto.loaders.unstructured import get_svo_tripplets, get_model
 from leto.storage.neo4j_storage import GraphStorage
 from leto.loaders.unstructured import Language
+import coreferee
+import subprocess
+
+def get_coreference_resolved_docs(nlp:spacy.Language, docs: Iterable[str]):
+    updated_docs = []
+    for doc in nlp.pipe(docs):
+        new_doc = []
+        for token in doc:
+            tok = doc._.coref_chains.resolve(token) or token
+            tok_text = ' and '.join([t.text for t in tok]) if(isinstance(tok, list)) else tok.text
+            tok_text += token.whitespace_
+            new_doc.append(tok_text)
+
+        updated_docs.append(''.join(new_doc))
+    return updated_docs
 
 def _seed_content(content: str, language: Language):
-    
     graph_db = GraphStorage()
     nlp = get_model(language)
 
-    for triplet in get_svo_tripplets(nlp, content):
+    ready_content = content
+    if (language is Language.en): #download english model
+        subprocess.run(["python3", "-m", "coreferee", "install", "en"])
+        nlp.add_pipe('coreferee')
+        ready_content = get_coreference_resolved_docs(nlp, [content])[0]
+
+    for triplet in get_svo_tripplets(nlp, ready_content):
         subject_str = " ".join(map(lambda x: str(x).strip().lower(), triplet.subject)).strip()
         verb_str = "_".join(map(lambda x: str(x.lemma_.strip().lower()), triplet.verb)).strip()
         object_str = " ".join(map(lambda x: str(x).strip().lower(), triplet.object)).strip()
@@ -45,16 +67,17 @@ def _seed_content(content: str, language: Language):
             except:
                 pass
         
-def seed_knowledge(wikipedia_page:str, language:Language = Language.en):
-    page = wikipedia.page(wikipedia_page)
+def seed_from_wikipedia(wikipedia_page_title:str, language:Language = Language.en):
+    page = wikipedia.page(wikipedia_page_title)
     _seed_content(page.content, language)
     
-def query_wikipedia(query:str):
+def query_wikipedia(query:str) :
     return wikipedia.search(query)
 
 """
 Example:
 
-seed_knowledge("October revolution")
-seed_knowledge("World War II")
+seed_from_wikipedia("October revolution")
+seed_from_wikipedia("World War II")
+seed_from_wikipedia("Lenin")
 """
