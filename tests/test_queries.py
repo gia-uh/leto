b3b6fc628d5c5f54b8d0920b5a526f96b85de98d
Alejandro Piad <apiad@apiad.net>
2021-10-11 08:10:58 -0400

    Add test

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pytest
 
 from leto.model import Entity
@@ -11,3 +12,16 @@ from leto.query import WhatQuery, WhoQuery, WhereQuery, WhichQuery, HowManyQuery
 )
 def test_spanish_query(query, result):
     assert SpanishRuleParser().parse(query) == result
+=======
+from leto.query.rules import EnglishRuleParser
+from leto.query import Query
+
+
+def test_basic_parser():
+    parser = EnglishRuleParser()
+    query_str = "show info about Cuba"
+    query = parser.parse(query_str)
+
+    assert isinstance(query, Query)
+    assert str(query.entities[0]) == "Cuba"
+>>>>>>> d616cde (Add test)
